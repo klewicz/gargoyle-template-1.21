44731c78aecbfce2397a5b19cba6286efc1ea322
Jan Kadlewicz <janek.kadlewicz@gmail.com>
2025-11-29 16:34:56 +0100

    Initial commit

---
+++ src/main/java/com/lewiczzz/gargoyle/Gargoyle.java
@@ -0,0 +1,15 @@
+package com.lewiczzz.gargoyle;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Gargoyle implements ModInitializer {
+	public static final String MOD_ID = "gargoyle";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
